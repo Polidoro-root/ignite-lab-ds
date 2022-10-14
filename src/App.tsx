579b9151875480c40c0799728a57bba88f37a806
Polidoro-root <jv.polidoro@outlook.com>
2022-10-13 21:12:09 -0300

    Storybook testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,8 @@
 import './styles/global.css'
-import { Envelope } from 'phosphor-react'
-import { Button } from './components/Button'
-import { Checkbox } from './components/Checkbox'
-import { Heading } from './components/Heading'
-import { Text } from './components/Text'
-import { TextInput } from './components/TextInput'
+import { SignIn } from './pages/SignIn'
 
 function App() {
-  return (
-    <div className="w-screen h-screen flex-col bg-gray-900 flex items-center justify-center text-gray-100">
-      <header className="flex flex-col items-center">
-        <Heading size="lg" className="mt-4">
-          Ignite Lab
-        </Heading>
-        <Text size="lg" className="text-gray-400 mt-1">
-          Faça login e comece a usar!
-        </Text>
-      </header>
-
-      <form className="flex flex-col items-stretch max-w-sm gap-y-4 mt-8">
-        <label htmlFor="email" className="flex flex-col gap-3">
-          <Text className="font-semibold">Endereço de email</Text>
-
-          <TextInput.Root>
-            <TextInput.Icon>
-              <Envelope />
-            </TextInput.Icon>
-
-            <TextInput.Input id="email" placeholder="Digite o seu email" />
-          </TextInput.Root>
-        </label>
-
-        <label htmlFor="password" className="flex flex-col gap-3">
-          <Text className="font-semibold">Sua senha</Text>
-
-          <TextInput.Root>
-            <TextInput.Icon>
-              <Envelope />
-            </TextInput.Icon>
-
-            <TextInput.Input
-              id="password"
-              type="password"
-              placeholder="*****"
-            />
-          </TextInput.Root>
-        </label>
-
-        <label htmlFor="remember" className="flex items-center gap-2">
-          <Checkbox id="remember" />
-          <Text size="sm" className="text-gray-200">
-            Lembrar de mim por 30 dias
-          </Text>
-        </label>
-
-        <Button type="submit" className="mt-4">
-          Entrar na plataforma
-        </Button>
-      </form>
-
-      <footer className="flex flex-col items-center gap-4 mt-8">
-        <Text asChild size="sm">
-          <a href="" className="text-gray-400 underline hover:text-gray-200">
-            Esqueceu sua senha?
-          </a>
-        </Text>
-
-        <Text asChild size="sm">
-          <a href="" className="text-gray-400 underline hover:text-gray-200">
-            Não possui uma conta? Crie agora!
-          </a>
-        </Text>
-      </footer>
-    </div>
-  )
+  return <SignIn />
 }
 
 export default App
